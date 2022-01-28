220bbb1c966f3474ef792219d59642a52721da93
George Lee <ghrlee@gmail.com>
2022-01-27 20:17:45 -0700

    add sliders

--- sketch.js
+++ sketch.js
@@ -8,13 +8,22 @@ const settings = {
   animate: true
 };
 
+const params = {
+  cols: 10,
+  rows: 10,
+  scaleMin: 1,
+  scaleMax: 30,
+  freq: 0.001,
+  amp: 0.2
+}
+
 const sketch = () => {
   return ({ context, width, height, frame }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const cols = 10;
-    const rows = 10;
+    const cols = params.cols;
+    const rows = params.rows;
     const numCells = cols * rows;
 
     const gridw = width * 0.8;
@@ -33,10 +42,11 @@ const sketch = () => {
       const y = row * cellh;
       const w = cellw * 0.8;
       const h = cellh * 0.8;
-      const n = random.noise2D(x + frame * 10, y, .001);
-      const angle = n * Math.PI * 0.2;
+      // const n = random.noise2D(x + frame * 10, y, params.freq);
+      const n = random.noise3D(x, y, frame * 10, params.freq);
+      const angle = n * Math.PI * params.amp;
       // const scale = (n + 1) / 2 * 30;
-      const scale = math.mapRange(n, -1, 1, 1, 30);
+      const scale = math.mapRange(n, -1, 1, params.scaleMin, params.scaleMax);
 
       context.save();
       context.translate(x, y);
@@ -60,6 +70,16 @@ const createPane = () => {
   let folder;
 
   folder = pane.addFolder({ title: 'Grid' });
+  folder.addInput(params, 'cols', { min: 2, max: 50, step: 1 })
+  folder.addInput(params, 'rows', { min: 2, max: 50, step: 1 })
+  folder.addInput(params, 'scaleMin', { min: 1, max: 100 })
+  folder.addInput(params, 'scaleMax', { min: 1, max: 100 })
+
+  folder = pane.addFolder({ title: 'Noise' });
+  folder.addInput(params, 'freq', { min: -0.01, max: 0.01 })
+  folder.addInput(params, 'amp', { min: 0, max: 1 })
+
+
 }
 
 createPane();
